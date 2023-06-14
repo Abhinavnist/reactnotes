23287bc971a99119ffe92100ecd05a9604452f36
Abhinav Kumar <Abhinavpatna55@gmail.com>
2023-06-15 01:04:30 +0530

    Updates

---
+++ precache-manifest.348e558688e6f537d9e5f9b913bd7bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34820b47908489296a8e02721aff1f99",
+    "url": "/reactnotes/index.html"
+  },
+  {
+    "revision": "77f78c4ab56afa82af82",
+    "url": "/reactnotes/static/css/main.cf524cb6.chunk.css"
+  },
+  {
+    "revision": "fe5d23542cec2d1260da",
+    "url": "/reactnotes/static/js/2.43be0641.chunk.js"
+  },
+  {
+    "revision": "77f78c4ab56afa82af82",
+    "url": "/reactnotes/static/js/main.f0912355.chunk.js"
+  },
+  {
+    "revision": "c887775d194fee05b1e6",
+    "url": "/reactnotes/static/js/runtime-main.51a717b7.js"
+  }
+]);
